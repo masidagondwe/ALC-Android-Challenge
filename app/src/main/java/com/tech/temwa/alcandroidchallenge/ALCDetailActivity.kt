e668cd7212f5feb3ce71b2b054c53891af5612b2
masidagondwe <masidahgondwe@gmail.com>
2019-07-16 22:00:14 +0200

    Updated alcdetail_activity

--- app/src/main/java/com/tech/temwa/alcandroidchallenge/ALCDetailActivity.kt
+++ app/src/main/java/com/tech/temwa/alcandroidchallenge/ALCDetailActivity.kt
@@ -9,57 +9,74 @@ import android.support.annotation.RequiresApi
 import android.view.MenuItem
 import android.webkit.*
 import android.widget.Toast
-import com.tech.temwa.alcandroidchallenge.R.id.webviewALCPage
 import kotlinx.android.synthetic.main.activity_alcdetail.*
+import android.webkit.WebView
+import android.graphics.Bitmap
+import android.view.View
+import android.widget.ProgressBar
+
 
 class ALCDetailActivity : AppCompatActivity() {
-
-    //var mywebview: WebView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alcdetail)
 
-       // setSupportActionBar(toolbar)
-       // supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-
         // Get the web view settings instance
         val settings = webviewALCPage.settings
-
-        // Enable java script in web view
-        settings.javaScriptEnabled = true
 
         // Enable and setup web view cache
         settings.setAppCacheEnabled(true)
         settings.cacheMode = WebSettings.LOAD_DEFAULT
         settings.setAppCachePath(cacheDir.path)
 
-        webviewALCPage.webViewClient = MyWebViewClient(this)
+
+        //webviewALCPage.webViewClient = MyWebViewClient(this)
+
+
+
+        webviewALCPage.webViewClient = object : WebViewClient() {
+
+            @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                val url: String = request?.url.toString()
+                view?.loadUrl(url)
+                return true
+            }
+
+            override fun onReceivedError(view: WebView, request: WebResourceRequest, error: WebResourceError) {
+                Toast.makeText(this@ALCDetailActivity, "Got Error! $error", Toast.LENGTH_SHORT).show()
+            }
+
+            @Override
+            override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
+                handler?.proceed()
+            }
+
+            override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                view.visibility =View.INVISIBLE
+                page_progress_bar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                super.onPageFinished(view, url)
+                view.visibility =View.VISIBLE
+                page_progress_bar.visibility = View.INVISIBLE
+            }
+        }
+
         webviewALCPage.loadUrl("https://andela.com/alc/")
 
-        /*  mywebview = findViewById<WebView>(R.id.webviewALCPage)
-          mywebview!!.webViewClient = object : WebViewClient() {
-              override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest ?): Boolean {
-                  val url = request?.getUrl().toString()
-                  view?.loadUrl(url)
-                  return true
-              }
-          }
-          mywebview!!.loadUrl("https://www.google.co.in/")*/
     }
 
-    class MyWebViewClient internal constructor(private val activity: Activity) : WebViewClient() {
+
+  /*  class MyWebViewClient internal constructor(private val activity: Activity) : WebViewClient() {
 
         @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
         override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-            val url: String = request?.url.toString();
+            val url: String = request?.url.toString()
             view?.loadUrl(url)
-            return true
-        }
-
-        @SuppressWarnings("deprecation")
-        override fun shouldOverrideUrlLoading(webView: WebView, url: String): Boolean {
-            webView.loadUrl(url)
             return true
         }
 
@@ -71,7 +88,9 @@ class ALCDetailActivity : AppCompatActivity() {
         override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
             handler?.proceed()
         }
-    }
+
+    } */
+
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         if (item!!.itemId == android.R.id.home){
